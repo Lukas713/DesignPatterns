62c89b8cfa6e4cb0267ed9d3697c9ce0687db715
Lukas <lukas.scharmitzer@gmail.com>
2019-02-25 15:08:20 +0100

    format and few omments

--- SimpleFactory.h
+++ SimpleFactory.h
@@ -14,74 +14,118 @@ class Product {
 protected:
     std::string name;
 public:
-    void methodOne(){
-        std::cout << "First method on " << name << "\n";
-    }
+    void methodOne();
+    virtual void methodTwo() = 0;
 };
 
 class ConcreteProductA : public Product {
 public:
-    ConcreteProductA(){
-        this->name = "Concrete product A";
-    }
+    ConcreteProductA();
+    void methodTwo() override;
 };
 
 class ConcreteProductB : public Product {
 public:
-    ConcreteProductB(){
-        this->name = "Concrete product B";
-    }
+    ConcreteProductB();
+    void methodTwo() override;
 };
 
 class ConcreteProductC : public Product {
 public:
-    ConcreteProductC(){
-        this->name = "Concrete product C";
-    }
+    ConcreteProductC();
+    void methodTwo() override;
 };
 
 class ConcreteProductD : public Product {
 public:
-    ConcreteProductD(){
-        this->name = "Concrete product D";
-    }
+    ConcreteProductD();
+    void methodTwo() override;
 };
 
 class ProductFactory {
 public:
-    Product* createFactory(PRODUCTS x){
-
-        Product* product = NULL;
-        switch(x){
-            case A:
-                product = new ConcreteProductA();
-                break;
-            case B:
-                product = new ConcreteProductB();
-                break;
-            case C:
-                product = new ConcreteProductC();
-                break;
-            case D:
-                product = new ConcreteProductD();
-                break;
-            default:
-                product = new ConcreteProductA();
-                break;
-        }
-        return product;
-    }
+    static Product* createFactory(PRODUCTS x);  //static method that decides which Abstract implementation will be used
 };
 
+/**
+ * Creator class that creates factory
+ */
 class Creator {
-    ProductFactory* factory;
 public:
-    Creator(ProductFactory* f){
-        this->factory = f;
-    }
-    void orderFactory(PRODUCTS x){
-        Product* product;
-        product = this->factory->createFactory(x);
-        product->methodOne();
-    }
+    void orderFactory(PRODUCTS x);  //invokes static factory method
 };
+
+
+/**
+ * #####################################
+ * METHOD DEFINITIONS
+ * #####################################
+ */
+void Creator::orderFactory(PRODUCTS x){
+    Product* product = NULL;
+    product = ProductFactory::createFactory(x);
+    product->methodOne();
+    product->methodTwo(); 
+}
+Product* ProductFactory::createFactory(PRODUCTS x){
+
+    Product* product = NULL;
+    switch(x){
+        case A:
+            product = new ConcreteProductA();
+            break;
+        case B:
+            product = new ConcreteProductB();
+            break;
+        case C:
+            product = new ConcreteProductC();
+            break;
+        case D:
+            product = new ConcreteProductD();
+            break;
+        default:
+            product = new ConcreteProductA();
+            break;
+    }
+    return product;
+}
+
+ConcreteProductD::ConcreteProductD(){
+    this->name = "Concrete product D";
+}
+
+void ConcreteProductD::methodTwo(){
+    std::cout << "Second, overriden method on " << name << "\n";
+    std::cout << "concrete product D\n";
+}
+
+ConcreteProductC::ConcreteProductC(){
+    this->name = "Concrete product C";
+}
+
+void ConcreteProductC::methodTwo(){
+    std::cout << "Second, overriden method on " << name << "\n";
+    std::cout << "concrete product C\n";
+}
+
+ConcreteProductB::ConcreteProductB(){
+    this->name = "Concrete product B";
+}
+
+void ConcreteProductB::methodTwo(){
+    std::cout << "Second, overriden method on " << name << "\n";
+    std::cout << "concrete product B\n";
+}
+
+ConcreteProductA::ConcreteProductA(){
+    this->name = "Concrete product A";
+}
+
+void ConcreteProductA::methodTwo(){
+    std::cout << "Second, overriden method on " << name << "\n";
+    std::cout << "concrete product A\n";
+}
+
+void Product::methodOne(){
+    std::cout << "First method on " << name << "\n";
+}
